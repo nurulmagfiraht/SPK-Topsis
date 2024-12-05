51343975635c30965a5b2c16e5e7883bd22a5aea
ahmadaqib <ahmadaqibsunu@gmail.com>
2024-12-06 03:43:36 +0800

    1.3.6

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Departemen;
+use App\Models\Karyawan;
+use App\Models\Outlet;
+use App\Models\Absensi;
+use App\Models\Divisi;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,6 +14,12 @@ class AdminController extends Controller
     public function index()
     {
         $departemenList = Departemen::all();
-        return view('admin.admin', compact('departemenList'));
+        $jumlahKaryawan = Karyawan::count();
+        $jumlahOutlet = Outlet::count();
+        $jumlahAbsensi = Absensi::count();
+        $jumlahDivisi = Divisi::count();
+        $jumlahDepartemen = Departemen::count();
+
+        return view('admin.admin', compact('departemenList', 'jumlahKaryawan', 'jumlahOutlet', 'jumlahAbsensi', 'jumlahDivisi', 'jumlahDepartemen'));
     }
 }
